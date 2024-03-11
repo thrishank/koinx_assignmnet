4a6561dc1b2c0e16ae4190218d8a5ec801bb6692
thrishank <thrishankkalluru@gmail.com>
2024-03-11 23:52:12 +0530

    added the navbar

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,17 +1,24 @@
-import logo from "./image.png";
-
+import { Heading } from "../components/heading";
+import logo from "../assets/logo.png";
 export function Navbar() {
   return (
     <>
-      <div className="bg-white">
-        <div>
-          <img src={logo}></img>
+      <div className="bg-white flex justify-between">
+        <div className="flex items-center px-8">
+          <img
+            src={logo}
+            className="h-10 p-2 flex items-center cursor-pointer"
+          />
         </div>
-        <div>
-          <h2>Crypto Taxes</h2>
-          <h2>Crypto Taxes</h2>
-          <h2>Crypto Taxes</h2>
-          <button className="bg-blue-500">Get Started</button>
+        <div className="flex justify-end items-center p-2 h-max align-middle">
+          <Heading text={"Crypto Taxes"} />
+          <Heading text={"Free Tools"} />
+          <Heading text={"Resource Center"} />
+          <div className="px-6">
+            <button className="bg-blue-600 text-white rounded-lg px-4 py-1">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </>
